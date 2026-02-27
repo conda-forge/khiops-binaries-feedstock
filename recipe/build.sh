eb02100bd698ce534691bb41fec516da876d4395
bruno <97033386+bruno-at-orange@users.noreply.github.com>
2026-02-27 10:37:53 +0100

    Add activate and deactivate files to fix MPICH slowness
    see https://github.com/conda-forge/mpich-feedstock/issues/132#issuecomment-3867319989

--- recipe/build.sh
+++ recipe/build.sh
@@ -21,3 +21,16 @@ cp ./build/conda/tmp/khiops_coclustering "$PREFIX/bin"
 
 # Copy header file
 cp ./src/Learning/KhiopsNativeInterface/KhiopsNativeInterface.h "$PREFIX/include"
+
+# Set up the activation and deactivation scripts for the Conda environment
+# We always copy the scripts under the "khiops-core" basename.  The recipe
+# declares those exact paths in the khiops-core output, so they will be
+# packaged there during the split.  The build script only runs once for the
+# entire multi‑output recipe, and $PKG_NAME refers to the top‑level package
+# (khiops-binaries), so using it in the filename would put the wrong name into
+# the prefix.
+for CHANGE in "activate" "deactivate"
+do
+    mkdir -p "${PREFIX}/etc/conda/${CHANGE}.d"
+    cp "${RECIPE_DIR}/${CHANGE}.sh" "${PREFIX}/etc/conda/${CHANGE}.d/khiops-core_${CHANGE}.sh"
+done
